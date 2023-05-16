8b904ffa0623c03ac2119bc719096f3e7d6a9a79
RIZKY EKA HARYADI <rizky28eka@MBP-RIZKY.bbrouter>
2023-05-17 04:50:37 +0700

    the program is already finished

---
+++ lib/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF020128);
+  static const Color FontColorWhite = Color(0xffcfcfcf);
+  static const Color FontColorPurple = Color(0xFF6200EE);
+  static const Color FontColorBlue = Color(0xFF2c3545);
+}
+
+
+// oxff030e22
